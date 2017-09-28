af53590653d72d648a064e62efdf1e6118d9d34b
darkblank <darkblank1990@gmail.com>
2017-09-28 17:33:01 +0900

    Question모델에 is_recently기능추가

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -1,13 +1,23 @@
+from datetime import timedelta
+
 from django.db import models
 
 
 # Create your models here.
+from django.utils import timezone
+
+
 class Question(models.Model):
     title = models.CharField(max_length=50)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f'설문조사 ({self.title})'
+
+    def is_recently(self):
+        return bool(self.published_date) and \
+               (timezone.now() - self.published_date <= timedelta(days=7))
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
