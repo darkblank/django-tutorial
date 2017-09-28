83ead2adc39640939bc02ede6e154eb66427a9ac
darkblank <darkblank1990@gmail.com>
2017-09-28 16:22:25 +0900

    Question, Choice 모델 생성, admin에 등록

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    title = models.CharField(max_length=50)
+    votes = models.IntegerField(default=0)
